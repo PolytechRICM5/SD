e2fff98d9995fd54195e3f7d4330c2160ff3a5aa
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2017-12-20 10:07:59 +0100

    Perceptron working + test

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -9,27 +9,6 @@ import numpy as np
 # Tout d'abord nous allons modifier le fichier iris.data de sorte que les iris-setosa appartiennent à la classe +1
 # et que les autres à la classe -1
 
-# S la base d'apprentissage
-# T le nombre max d'itérations
-# eta le pas d'apprentissage
-def perceptron (S, T, eta) :
-    size_of_space = len(S[0]) -1
-    w0 = [0]
-    w = [ [0] * size_of_space ]
-    for t = 1..T :
-        for i = 1..len(S) :
-            yi = S[i][4]
-            if  yi * (w0[t] + <w[t],xi>) <= 0 :
-                w0[t+1] <- w0[t] + eta * yi
-                w[t+1] <- w[t] + eta * xi *yi
-    print w0
-
-# BT la base de test
-# w0
-# w
-def testPerceptron(BT, w0, w) :
-    print hi
-
 def getIris () :
     fname = "iris.data"
     f = open(fname, "r")
@@ -42,7 +21,43 @@ def getIris () :
         else :
             line_tab[4] = "-1"
         lines.append(line_tab)
-    return lines
+    return np.array(lines, dtype=float) # Convertir le contenu du tableau en float
+
+# S la base d'apprentissage
+# T le nombre max d'itérations
+# eta le pas d'apprentissage
+def perceptron (S, T, eta) :
+    size_of_space = len(S[0]) -1
+    w0 = np.zeros((T+1,1))
+    w = np.zeros((T+1,size_of_space))
+    for t in range(0,T) :
+        for i in range(0,len(S)) :
+            yi = S[i][size_of_space]
+            if  yi * (w0[t] + np.dot(w[t], S[i][0:size_of_space])) <= 0 :
+                w0[t+1] = w0[t] + eta * yi
+                w[t+1] = w[t] + eta * S[i][0:size_of_space] * yi
+    return w[T]
+
+# BT la base de test
+# wT le résultat du perceptron
+def testPerceptron(BT, wT) :
+    size_of_space = len(BT[0]) -1
+    perf = 0
+    for i in range(0,len(BT)) :
+        yi = BT[i][size_of_space]
+        if yi * np.dot(wT, BT[i][0:size_of_space]) :
+            perf += 1
+    return perf/len(BT)
+
+# S le set de données
+# k le nombre de sous-ensembles sur lesquels faire la cross-validation
+def choixEta(S,k) :
+    size = len(S)
+    eta_range = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+    res = []
+    for eta in eta_range :
+        print "Résultat pour eta = " + eta
+
 
 
 # Récupération des données, mélange et répartition en un set de test et un set d'apprentissage
@@ -53,7 +68,10 @@ test_list = data[0:cut]
 learn_list = data[cut:len(data)]
 
 # Lancement de l'algorithme de perceptron renvoyant w0 et w.
-perceptron(learn_list,100,0.1)
-
+res = perceptron(learn_list,100,0.1)
+perf = testPerceptron(test_list,res)
+print res
+print perf
+choixEta(data,4)
 
 # http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
